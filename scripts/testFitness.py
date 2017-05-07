b5b8adff1020a1c1a84b59450b3bad254db6fec3
dfbarrero <david.barrero@gmail.com>
2017-05-07 13:29:17 +0200

    File clean

--- scripts/testFitness.py
+++ scripts/testFitness.py
@@ -1,26 +1,8 @@
 #!/usr/bin/env python
 
-import rospy
-
-from geometry_msgs.msg import *
-
 from subprocess import call
 
-#rospy.init_node("simulationServer")
-
-#try:
-	#pose = Pose2D()
-	#pose.x = 10
-	#pose.y = 10
-	#pose.theta = 0
-	#reset = rospy.ServiceProxy("/robot0/replace", pose)
-	#response = reset(pose)
 arg = "".join(str(range(18))).replace(" ", "")
 res = call(["rosservice", "call", "/computeFitness", arg])
 print(res)
-#except rospy.ServiceException:
-#	print "Service call failed"
 
-
-#rospy.spin()
-#
