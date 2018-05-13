a5aac3f1fb35ca43f79cb12e2ac1c438d36ea30e
David <david.barrero@gmail.com>
2018-05-13 11:26:21 -0500

    Neurocontroller modified

--- scripts/neurocontroller.py
+++ scripts/neurocontroller.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 import rospy
-import numpy
 import math
 
 from geometry_msgs.msg import Twist
@@ -12,24 +11,21 @@ from nav_msgs.msg import Odometry
 
 from ros_stdr_ga.srv import *
 
-from pybrain.structure import FeedForwardNetwork, SigmoidLayer, LinearLayer, FullConnection
-from pybrain.tools.shortcuts import buildNetwork
-
-from pybrain.tools.xml.networkwriter import NetworkWriter
-
 from subprocess import call
 
-
-# Parameters
+# Parameters, do not touch it
 N_SONAR = 4 # Number of sonars in the robot
 
 LINEAR_MUL = 2 # Used to denormalize ANN output
 ANGULAR_MUL = 2
 
 ranges = [0] * N_SONAR
-odom = [0] * 2
+odom = [None] * 2
+initX = 10
+initY = 1.5
 
-ann = 0
+ann = None
+distance = 0 
 
 def callbackSonar(msg, arg):
 	#rospy.loginfo("%f: [%f]"%(arg, msg.range))
@@ -39,10 +35,21 @@ def callbackSonar(msg, arg):
 	ranges[arg] = msg.max_range / msg.range # Normalize ranges, needed to feed the ANN
 
 def callbackOdom(msg):
+	global distance
+	global odom
+
+	if (odom[0] is None): # Init odometry to properly compute distance
+		odom[0] = msg.pose.pose.position.x
+		odom[1] = msg.pose.pose.position.y
+		
+	distance = distance + math.sqrt(math.pow((odom[0]-msg.pose.pose.position.x),2) + math.pow((odom[1]-msg.pose.pose.position.y),2))
 	odom[0] = msg.pose.pose.position.x
 	odom[1] = msg.pose.pose.position.y
+	#print ("Distance: " + str(distance))
 
 def initANN():
+	global ann
+
 	rospy.loginfo("Building network")
 	# ANN with  N_SONAR inputs and
 	# two outputs for linear and angular velocities
@@ -50,37 +57,35 @@ def initANN():
 	# TODO: Initialize network topology. Use the global variable ann to refer the network
 	# The ANN *must* define N_SONAR input neurons and two output neurons
 
-	return ann
+	rospy.loginfo("You must initialize your ANN, it will crash otherwise") # Your can remove this line safely
 
 def controlLoop(weights):
 	global ann
-	rospy.loginfo("Weights needed: " + str(len(ann.params)) + ", got " + str(len(weights)))
-
-	if (len(ann.params) != len(weights)): rospy.loginfo("WARN: Bad size")
-	ann._setParameters(weights)
+	global distance
 
 	vel = Twist()
 	iterations = 0
 
 	rate = rospy.Rate(10)
-	while (not rospy.is_shutdown()) and iterations < 20: # 20 iterations seems enoght
-		out = ann.activate(ranges)
+	while (not rospy.is_shutdown()) and iterations < 30: # 30 iterations seems enougth
+		# TODO: Feed the ANN with global variable ranges and store its output as an array of two floats
+		out = None
 
+		# Move the robot according to the ANN output
 		vel.linear.x = out[0] * LINEAR_MUL
 		vel.angular.z = out[1] * ANGULAR_MUL
 		velTopic.publish(vel)
 		iterations = iterations + 1
-		#rospy.loginfo(iterations)
 		rate.sleep()
+
 	# Stop robot motion
 	vel.linear.x = 0
 	vel.angular.z = 0
 	velTopic.publish(vel)
 
-	fit = math.sqrt((math.pow(1-odom[0], 2)+ math.pow(1-odom[1], 2)))
+	# Compute fitness
+	fit = distance + math.sqrt(math.pow(odom[0]-initX, 2) + math.pow(odom[1]-initY,2))
 	rospy.loginfo("Fitness: " + str(fit))
-	#ann._setParameters([0] * len(ann.params))
-	ann = initANN()
 	return(fit)
 
 def handle_computeFitness(req):
@@ -91,28 +96,26 @@ def handle_computeFitness(req):
 	vel.angular.z = 0
 	velTopic.publish(vel)
 
+	# Reset distance
+	global distance
+	distance = 0
+
 	rospy.loginfo("Init neurocontrol")
 	weights = []
 	for i in req.ann: weights.append(i)
 	rospy.loginfo(weights)
 	# Reset simulation
 	try:
-		call(["rosservice", "call", "/robot0/replace", "[1,1,1]"]) # TODO: Fix this 
+		call(["rosservice", "call", "/robot0/replace", "[10,1.5,0]"]) # I know, nasty code, fix it 
 		fitness = controlLoop(weights)
-		#reset = rospy.ServiceProxy("/robot0/replace", Pose2D)
-		#pose = Pose2D()
-		#pose.x = 10
-		#pose.y = 10
-		#pose.theta = 0
-		#response = reset(pose)
-		#print(pose)
 	except rospy.ServiceException:
 		print "Service call failed"
 
 	return fitness
 
 if __name__ == '__main__':
-	rospy.init_node('ga_controller')
+	global ann
+	rospy.init_node('neurocontroller')
 	
 	# Stop robot motion
 	# (Needed because of previous runs)
@@ -122,7 +125,7 @@ if __name__ == '__main__':
 	vel.angular.z = 0
 	velTopic.publish(vel)
 
-	ann = initANN()
+	initANN()
 
 	for i in range(N_SONAR):
 		rospy.Subscriber("/robot0/sonar_"+str(i), Range, callbackSonar, i)
